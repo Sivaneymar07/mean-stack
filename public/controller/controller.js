d7649b3be4edd8ecda898d047bc33e58fef42a5b
Sivaneymar07 <sivayogesh72@gmail.com>
2017-02-09 15:52:18 +0530

    Add files via upload

---
+++ public/controller/controller.js
@@ -0,0 +1,43 @@
+var app = angular.module("myApp", []);
+
+app.controller("AppCtrl", function($scope, $http) {
+	var refresh = function(){
+  $http.get("/contactlist").success(function(response) {
+  	console.log("I got the data");
+        $scope.contactlist = response;
+        $scope.contact="";
+    });
+
+};
+refresh();
+
+$scope.addContact = function(){
+	console.log($scope.contact);
+	$http.post("/contactlist",$scope.contact).success(function(response){
+	console.log(response);
+	refresh();
+	});
+}
+$scope.remove = function(id){
+	console.log(id);
+	$http.delete('/contactlist/' + id).success(function(response){
+		refresh();
+	});
+}
+
+	$scope.edit = function(id){
+	console.log(id);
+	$http.get('/contactlist/'+id).success(function(response){
+		$scope.contact = response;
+
+	});
+}
+
+
+	$scope.update = function(id){
+	console.log(id);
+	$http.put('/contactlist/' + $scope.contact._id,$scope.contact)
+};
+
+
+});
